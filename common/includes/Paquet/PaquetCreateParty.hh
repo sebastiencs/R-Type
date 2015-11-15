bf8a537cb5a39e26f38f772b76e9e6664136b5d3
chapui_s <chapui_s@epitech.eu>
2015-11-15 02:55:59 +0100

    Implement Paquet create party

---
+++ common/includes/Paquet/PaquetCreateParty.hh
@@ -0,0 +1,36 @@
+//
+// PaquetCreateParty.hh for epitech in /home/chapui_s/rendu/
+//
+// Made by chapui_s
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Sun Nov 15 02:20:11 2015 chapui_s
+// Last update Sun Nov 15 02:20:11 2015 chapui_s
+//
+
+#ifndef PAQUETCREATEPARTY_H_
+# define PAQUETCREATEPARTY_H_
+
+# include "Paquet.hh"
+
+class		PaquetCreateParty : public Paquet
+{
+private:
+  uint8_t	_id;
+  std::string	_name;
+
+public:
+  PaquetCreateParty();
+  PaquetCreateParty(void *, size_t);
+  virtual ~PaquetCreateParty();
+
+  void			setName(const std::string &);
+  const std::string	getName() const;
+
+  void			createPaquet();
+  void			parsePaquet();
+};
+
+std::ostream	&operator<<(std::ostream &, PaquetCreateParty &);
+
+#endif /* !PAQUETCREATEPARTY_H_ */
