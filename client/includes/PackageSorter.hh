aa4e5d3372f856339b94f5d9b40d9c9030f2d57b
Alex <alex.dana@epitech.eu>
2015-11-17 15:26:20 +0100

    * PackageSorter create (.hh a .cpp)

---
+++ client/includes/PackageSorter.hh
@@ -0,0 +1,28 @@
+//
+// PackageSorter.cpp for epitech in /home/chapui_s/rendu/
+//
+// Made by chapui_s
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Sun Nov 15 02:08:19 2015 chapui_s
+// Last update Sun Nov 15 02:08:19 2015 chapui_s
+//
+
+#ifndef PACKAGE_SORTER_HH
+# define PACKAGE_SORTER_HH
+
+# include <iostream>
+# include <list>
+# include "PackageStorage.hh"
+
+class PackageSorter {
+public:
+  PackageSorter();
+  PackageSorter(PackageStorage *storage);
+  virtual ~PackageSorter();
+
+private:
+  PackageStorage *storage;
+};
+
+#endif /* !PACKAGE_SORTER_HH */
---
+++ client/src/PackageSorter.cpp
@@ -0,0 +1,24 @@
+//
+// PackageSorter.cpp for epitech in /home/chapui_s/rendu/
+//
+// Made by chapui_s
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Sun Nov 15 02:08:19 2015 chapui_s
+// Last update Sun Nov 15 02:08:19 2015 chapui_s
+//
+
+#include "..\includes\PackageSorter.hh"
+
+PackageSorter::PackageSorter()
+{
+  storage = new PackageStorage();
+}
+
+PackageSorter::PackageSorter(PackageStorage * storage) : storage(storage)
+{
+}
+
+PackageSorter::~PackageSorter()
+{
+}
