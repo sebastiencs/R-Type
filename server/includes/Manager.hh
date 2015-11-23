30a12437abc9f7a13e3c88ad6b77105586b8d492
Sebastien Chapuis <seb@chapuis.co>
2015-11-23 17:52:04 +0100

    Create Manager class

---
+++ server/includes/Manager.hh
@@ -0,0 +1,22 @@
+//
+// Manager.hh for epitech in /home/chapui_s/rendu/
+//
+// Made by chapui_s
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Mon Nov 23 17:47:34 2015 chapui_s
+// Last update Mon Nov 23 17:47:34 2015 chapui_s
+//
+
+#ifndef MANAGER_H_
+# define MANAGER_H_
+
+class		Manager
+{
+public:
+  Manager();
+  virtual ~Manager();
+};
+
+
+#endif /* !MANAGER_H_ */
---
+++ server/src/Manager.cpp
@@ -0,0 +1,19 @@
+//
+// Manager.cpp for epitech in /home/chapui_s/rendu/
+//
+// Made by chapui_s
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Mon Nov 23 17:49:14 2015 chapui_s
+// Last update Mon Nov 23 17:49:14 2015 chapui_s
+//
+
+#include "Manager.hh"
+
+Manager::Manager()
+{
+}
+
+Manager::~Manager()
+{
+}
