1d00d1958dd5fdaa36ecdc55548da7b6f6506382
sebastien <seb93250@gmail.com>
2015-11-23 12:44:54 +0100

    Create semaphore files (Win)

---
+++ common/includes/SemaphoreWin.hh
@@ -0,0 +1,31 @@
+//
+// SemaphoreWin.hh for epitech in /home/chapui_s/rendu/
+//
+// Made by chapui_s
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Mon Nov 23 12:42:07 2015 chapui_s
+// Last update Mon Nov 23 12:42:07 2015 chapui_s
+//
+
+#ifndef SEMAPHOREWIN_H_
+# define SEMAPHOREWIN_H_
+
+# include "ISemaphore.hh"
+# include "Debug.hh"
+
+class		SemaphoreWin : public ISemaphore
+{
+private:
+  // sem_t		_sem;
+
+public:
+  SemaphoreWin();
+  virtual ~SemaphoreWin();
+
+  virtual bool	post();
+  virtual bool	wait();
+  virtual bool	tryWait();
+};
+
+#endif /* !SEMAPHOREWIN_H_ */
---
+++ common/src/SemaphoreWin.cpp
@@ -0,0 +1,34 @@
+//
+// SemaphoreWin.cpp for epitech in /home/chapui_s/rendu/
+//
+// Made by chapui_s
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Wed Nov 18 22:25:17 2015 chapui_s
+// Last update Wed Nov 18 22:25:17 2015 chapui_s
+//
+
+#include "SemaphoreWin.hh"
+
+SemaphoreWin::SemaphoreWin()
+{
+}
+
+SemaphoreWin::~SemaphoreWin()
+{
+}
+
+bool	SemaphoreWin::post()
+{
+  return (true);
+}
+
+bool	SemaphoreWin::wait()
+{
+  return (true);
+}
+
+bool	SemaphoreWin::tryWait()
+{
+  return (true);
+}
