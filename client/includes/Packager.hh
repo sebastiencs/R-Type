bcfea5823f8c9add83ae88e2167b6a099e1ef77d
Joseph Barbisan <joseph.barbisan@outlook.com>
2015-11-23 12:31:29 +0100

    /includes/

--- client/includes/Packager.hh
+++ client/includes/Packager.hh
@@ -1,13 +1,31 @@
 #ifndef PACKAGER_HH
 #define PACKAGER_HH
 
+#include <stdint.h>
+#include "PaquetPlayerCoord.hh"
+#include "PaquetPlayerShot.hh"
+#include "PackageStorage.hh"
+#include "PaquetRequestParties.hh"
+#include "PaquetRequestPlayers.hh"
+#include "PaquetJoinParty.hh"
+#include "PaquetCreateParty.hh"
+
 class Packager {
 public:
-	Packager();
+	Packager(PackageStorage *_storage);
 	virtual ~Packager();
 
-	void createMovementPackage();
-	void createShotPackage();
+	void createMovementPackage(uint8_t _playerID, uint8_t _x,	uint8_t _y);
+	void createShotPackage(uint8_t _playerID, uint8_t _type, uint8_t _x, uint8_t _y);
+	
+	void createGameListPackage();
+	void createPlayerListPackage();
+
+	void createJoinPartyPackage(std::string	_name);
+	void createCreatePartyPackage(std::string	_name);
+
+private:
+	PackageStorage *storage;
 };
 
 #endif PACKAGER_HH
