1336eac7b32eac9f6e697095e48b185de6f3bc56
Alex <alex.dana@epitech.eu>
2015-11-23 14:48:50 +0100

    * chg: remove a lot of useless line in PackageTranslator.cpp ....

--- client/src/PackagerTranslator.cpp
+++ client/src/PackagerTranslator.cpp
@@ -17,31 +17,9 @@ PackageTranslator::~PackageTranslator()
 
 Paquet * PackageTranslator::BufferToPaquet(const Buffer& buff)
 {
-  std::function<Paquet *(const Buffer&)> tab[13];
-  tab[0] = [this](const Buffer& buff) {return (new PaquetFirst(buff)); };
-  tab[1] = [this](const Buffer& buff) {return (new PaquetRequestParties(buff)); };
-  tab[2] = [this](const Buffer& buff) {return (new PaquetListPlayers(buff)); };
-  tab[3] = [this](const Buffer& buff) {return (new PaquetJoinParty(buff)); };
-  tab[4] = [this](const Buffer& buff) {return (new PaquetCreateParty(buff)); };
-  tab[5] = [this](const Buffer& buff) {return (new PaquetRequestPlayers(buff)); };
-  tab[6] = [this](const Buffer& buff) {return (new PaquetListPlayers(buff)); };
-  tab[7] = [this](const Buffer& buff) {return (new PaquetPlayerCoord(buff)); };
-  tab[8] = [this](const Buffer& buff) {return (new PaquetPlayerShot(buff)); };
-  tab[9] = [this](const Buffer& buff) {return (new PaquetObstacle(buff)); };
-  tab[10] = [this](const Buffer& buff) {return (new PaquetReady(buff)); };
-  tab[11] = [this](const Buffer& buff) {return (new PaquetLaunch(buff)); };
-  tab[12] = [this](const Buffer& buff) {return (new PaquetLeave(buff)); };
+  Paquet*	paquet = new Paquet(buff.get(), buff.size());
 
-  // size_t	ptr = 0;
-  uint8_t id;
-
-  Paquet	paquet(buff.get(), buff.size());
-
-  id = paquet.getData()[0];
-
-  // writeData<char>(ptr, reinterpret_cast<const char *>(buff.get()), buff.size());
-  // id = readData<uint8_t>(ptr);
-  return tab[id](buff);
+  return paquet;
 }
 
 Buffer * PackageTranslator::PaquetToBuffer(const Paquet& paquet)
