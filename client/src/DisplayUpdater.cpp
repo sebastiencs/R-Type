0b647d550219aa0a7cf2af51c97a46b27454c4fe
Joseph Barbisan <joseph.barbisan@outlook.com>
2015-11-24 18:45:33 +0100

    displayUpdater example

--- client/src/DisplayUpdater.cpp
+++ client/src/DisplayUpdater.cpp
@@ -1,9 +1,60 @@
 #include "DisplayUpdater.hh"
 
-DisplayUpdater::DisplayUpdater()
+
+DisplayUpdater::DisplayUpdater(Packager * _packager)
 {
+	packager = _packager;
+	graphicEngine = new GraphicEngine(packager);
+	graphicEngine->createWindow(800, 600, "R-Type");
+
+	graphicEngine->launch();
 }
 
 DisplayUpdater::~DisplayUpdater()
 {
+}
+
+void DisplayUpdater::inMenu()
+{
+	//graphicEngine.setCallBackFunction(&mainMenu);
+}
+
+void DisplayUpdater::inGame()
+{
+	//graphicEngine.setCallBackFunction(&game);
+}
+
+const Packager * DisplayUpdater::getPackager()
+{
+	return packager;
+}
+
+IGraphicEngine * DisplayUpdater::getGraphicEngine()
+{
+	return graphicEngine;
+}
+
+void mainMenu(void *arg) {
+	//drawButton("play");
+	//drawButton("exit");
+}
+
+void game(void *arg) {
+	DisplayUpdater *tmp = static_cast<DisplayUpdater *>(arg);
+
+	/*while (PackageStorage::getInstance().getObstaclesPackage() != nullptr) {
+		const PaquetObstacle* p = PackageStorage::getInstance().getObstaclesPackage();
+		tmp->getGraphicEngine()->drawImage(obstacleTypeToSpriteString[p->getType()], Transformation(p->getX(), p->getY()));
+		PackageStorage::getInstance().deleteObstaclesPackage();
+	}
+	while (PackageStorage::getInstance().getPlayersPackage() != nullptr) {
+		const PaquetPlayerCoord* p = PackageStorage::getInstance().getPlayersPackage();
+		tmp->getGraphicEngine()->drawImage(playerIDToSpriteString[p->getPlayerID()], Transformation(p->getX(), p->getY()));
+		PackageStorage::getInstance().deletePlayersPackage();
+	}
+	while (PackageStorage::getInstance().getShotsPackage() != nullptr) {
+		const PaquetPlayerShot* p = PackageStorage::getInstance().getShotsPackage();
+		tmp->getGraphicEngine()->drawImage(shotTypeToSpriteString[p->getType()], Transformation(p->getX(), p->getY()));
+		PackageStorage::getInstance().deleteShotsPackage();
+	}*/
 }
