37c9d41b00d7ff16697d00509864e5984d43c911
MisterAlex95 <alex.dana@epitech.eu>
2015-12-06 23:53:28 +0100

    change some code

--- client/src/LobbyMenu.cpp
+++ client/src/LobbyMenu.cpp
@@ -159,6 +159,7 @@ void LobbyMenu::updatePlayerList()
 {
 	quadPlayerBox->clearElements();
 	ListPlayers& playerList = ListPlayers::getInstance();
+	std::string ready;
 	size_t t = 0;
 	for (Player* p : playerList.getListPlayers()) {
 		Transformation tr(0, 0);
@@ -167,7 +168,11 @@ void LobbyMenu::updatePlayerList()
 		tr.setScale((float)1, (float)1);
 		TextField* playerName = new TextField(p->getName(), tr, DEFAULT_FONT_SIZE + 10, DEFAULT_FONT, Color::White, p->getName(), engine);
 		TextField* playerLVL = new TextField("0", tr, DEFAULT_FONT_SIZE + 10, DEFAULT_FONT, Color::White, "LVL", engine);
-		TextField* playerStatus = new TextField("Unready", tr, DEFAULT_FONT_SIZE + 10, DEFAULT_FONT, Color::Red, ((p->getReady()) ? "Ready" : "Unready"), engine);
+		if (p->getReady())
+			ready = "Ready";
+		else
+			ready = "Unready";
+		TextField* playerStatus = new TextField("Unready", tr, DEFAULT_FONT_SIZE + 10, DEFAULT_FONT, Color::Red, ready, engine);
 		Box* box = new Box(Orientation::horizontal, Transformation(200, 200), "Player" + std::to_string(p->getID()) + "Box");
 		box->setSpacing(50);
 		box->addDrawable(playerVessel);
