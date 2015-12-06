c24514b91600e671206e68afc81864396de9f791
Joseph Barbisan <joseph.barbisan@outlook.com>
2015-12-06 19:09:23 +0100

    small fix to taskscheduler

--- common/src/TaskScheduler.cpp
+++ common/src/TaskScheduler.cpp
@@ -7,6 +7,15 @@ TaskScheduler::TaskScheduler(Callback_t _callback, long _t)
 	Callback_t fptr = [this](void *) {this->loop(); return nullptr; };
 	thread = new Thread(fptr, nullptr);
 	timer = new Timer();
+	thread->run(fptr, nullptr);
+}
+
+TaskScheduler::~TaskScheduler()
+{
+	thread->join();
+	thread->close();
+	delete thread;
+	delete timer;
 }
 
 void TaskScheduler::loop()
