7546a4cb018eabc53a8230fe88b5f0ac159b2748
MisterAlex95 <alex.dana@epitech.eu>
2015-12-07 00:12:24 +0100

    Change color too

--- client/src/LobbyMenu.cpp
+++ client/src/LobbyMenu.cpp
@@ -61,8 +61,9 @@ void LobbyMenu::createRequestListPlayersPaquet()
 		ListPlayers &LP = ListPlayers::getInstance();
 
 		const PaquetListPlayers	*tmp = nullptr;
+		const PaquetReady	*tmp2 = nullptr;
 
-		while (!tmp) {
+		while (!tmp && !tmp2) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(50));
 			if ((tmp = PS.getPlayerListPackage())) {
 				LP.clearList();
@@ -74,14 +75,9 @@ void LobbyMenu::createRequestListPlayersPaquet()
 				DEBUG_MSG("Request received");
 				this->setPlayerListChanged(true);
 			}
-		}
 
-		const PaquetReady	*tmp2 = nullptr;
-
-		while (!tmp2) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
 			if ((tmp2 = PS.getReadyPackage())) {
-				LP.clearList();
+				// LP.clearList();
 				for (auto p : tmp->getPlayers()) {
 					LP.getPlayer(tmp2->getID())->setReady(true);
 				}
@@ -166,6 +162,7 @@ void LobbyMenu::updatePlayerList()
 	quadPlayerBox->clearElements();
 	ListPlayers& playerList = ListPlayers::getInstance();
 	std::string ready = "Unready";
+	Color cReady = Color::Red;
 	size_t t = 0;
 	for (Player* p : playerList.getListPlayers()) {
 		Transformation tr(0, 0);
@@ -174,9 +171,11 @@ void LobbyMenu::updatePlayerList()
 		tr.setScale((float)1, (float)1);
 		TextField* playerName = new TextField(p->getName(), tr, DEFAULT_FONT_SIZE + 10, DEFAULT_FONT, Color::White, p->getName(), engine);
 		TextField* playerLVL = new TextField("0", tr, DEFAULT_FONT_SIZE + 10, DEFAULT_FONT, Color::White, "LVL", engine);
-		if (p->getID())
+		if (p->getID()) {
 			ready = "Ready";
-		TextField* playerStatus = new TextField(ready, tr, DEFAULT_FONT_SIZE + 10, DEFAULT_FONT, Color::Red, "Ready", engine);
+			cReady = Color::Green;
+		}
+		TextField* playerStatus = new TextField(ready, tr, DEFAULT_FONT_SIZE + 10, DEFAULT_FONT, cReady, "Ready", engine);
 		Box* box = new Box(Orientation::horizontal, Transformation(200, 200), "Player" + std::to_string(p->getID()) + "Box");
 		box->setSpacing(50);
 		box->addDrawable(playerVessel);
