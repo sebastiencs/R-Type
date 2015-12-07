e49e8a2e967e6c6fb686716bfda0c3c51f42c819
MisterAlex95 <alex.dana@epitech.eu>
2015-12-07 22:28:53 +0100

    Add paquet.*

---
+++ common/src/Paquet/PaquetFirstUDP.cpp
@@ -0,0 +1,67 @@
+//
+// PaquetFirstUDP.cpp for epitech in /home/chapui_s/rendu/
+//
+// Made by chapui_s
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Fri Nov 13 21:47:15 2015 chapui_s
+// Last update Fri Nov 13 21:47:15 2015 chapui_s
+//
+
+#include "PaquetFirstUDP.hh"
+
+PaquetFirstUDP::PaquetFirstUDP()
+	: _id(Paquet::FIRST),
+		_playerId(0)
+{
+}
+
+PaquetFirstUDP::PaquetFirstUDP(const Buffer &buf)
+{
+  size_t	ptr = 0;
+
+  writeData<char>(ptr, reinterpret_cast<const char *>(buf.get()), buf.size());;
+  parsePaquet();
+}
+
+PaquetFirstUDP::~PaquetFirstUDP()
+{
+}
+
+void PaquetFirstUDP::setId(uint8_t id)
+{
+	_playerId = id;
+}
+
+uint8_t PaquetFirstUDP::getId() const
+{
+	return _playerId;
+}
+
+void			PaquetFirstUDP::parsePaquet()
+{
+  size_t	ptr = 0;
+
+  _id = readData<uint8_t>(ptr);
+	_playerId = readData<uint8_t>(ptr);
+}
+
+void			PaquetFirstUDP::createPaquet()
+{
+  size_t	ptr = 0;
+
+  writeData<uint8_t>(ptr, &_id);
+  writeData<uint8_t>(ptr, &_playerId);
+}
+
+std::ostream	&operator<<(std::ostream &os, PaquetFirstUDP &p)
+{
+  p.parsePaquet();
+
+  int		id = p.getId();
+
+  os << "PaquetFirstUDP = {"
+     << " _id : '" << id
+     << " };";
+  return (os);
+}
