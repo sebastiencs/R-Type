931357c2a108d4c98fafb3fd1b927f464314fe6a
MisterAlex95 <alex.dana@epitech.eu>
2015-12-09 16:13:32 +0100

    remove abs

--- common/src/Patern.cpp
+++ common/src/Patern.cpp
@@ -25,11 +25,6 @@ uint8_t Patern::applyPatern(uint8_t x)
 	return x;
 }
 
-uint8_t absPatern(uint8_t x)
-{
-	return (uint8_t)std::abs(x);
-}
-
 uint8_t cosPatern(uint8_t x)
 {
 	return (uint8_t)std::cos(x);
