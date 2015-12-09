4dec493a3dfdf6f084282e125405b185aa0f34e5
Boris Bresciani <boris.bresciani@epitech.eu>
2015-12-09 22:23:33 +0100

    begin Wave class

---
+++ common/includes/Wave.hh
@@ -0,0 +1,31 @@
+//
+// Enemy.hh for epitech in /home/bresci_b/R-Type
+//
+// Made by bresci_b
+// Login   <bresci_b@epitech.eu>
+//
+// Started on  Fri Nov 13 00:37:48 2015 bresci_b
+// Last update Fri Nov 13 00:37:48 2015 bresci_b
+//
+
+#ifndef WAVE_HH_
+# define WAVE_HH_
+
+# include <iostream>
+# include <memory>
+# include <map>
+# include "Enemy.hh"
+# include "Timer.h"
+# include "Debug.hh"
+
+class		Wave
+{
+public:
+  Wave();
+  virtual ~Wave();
+
+private:
+  std::map<Timer*, Enemy*>	_enemies
+};
+
+#endif /* !WAVE_HH_ */
---
+++ common/src/Wave.cpp
@@ -0,0 +1,21 @@
+//
+// Wave.cpp for epitech in /home/bresci_b/R-Type
+//
+// Made by bresci_b
+// Login   <bresci_b@epitech.eu>
+//
+// Started on  Fri Nov 13 00:37:48 2015 bresci_b
+// Last update Fri Nov 13 00:37:48 2015 bresci_b
+//
+
+#include "Wave.hh"
+
+Wave::Wave()
+{
+  DEBUG_MSG("Wave created");
+}
+
+Wave::~Wave()
+{
+  DEBUG_MSG("Wave deleted");
+}
