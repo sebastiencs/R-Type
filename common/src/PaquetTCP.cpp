ea3d46ed065c382debc9eee073d9628892cdd414
Sebastien Chapuis <seb@chapuis.co>
2015-12-12 02:31:09 +0100

    handle PaquetBonusMalus in PaquetTCP class

--- common/src/PaquetTCP.cpp
+++ common/src/PaquetTCP.cpp
@@ -57,6 +57,7 @@ void	PaquetTCP::initSize()
   _pSize[Paquet::RESPONSE]		= 0x06;
   _pSize[Paquet::FIRST_UDP]		= 0x02;
   _pSize[Paquet::ENEMY]			= 0x08;
+  _pSize[Paquet::BONUS_MALUS]		= 0x08;
 }
 
 ISocketTCP	*PaquetTCP::accept()
