74141a1ed0ccd6c45a7a05e2c76e3c8e970efe75
Sebastien Chapuis <seb@chapuis.co>
2015-12-13 06:40:14 +0100

    Wave attribute action to the enemy

--- server/src/Wave.cpp
+++ server/src/Wave.cpp
@@ -32,24 +32,44 @@ void		Wave::getSpawn()
   switch(nb_waves)
   {
   case 2: // 2 enemies
-    while(++i < 2) {
-      _party.addEnemy(new Enemy(_party.getUniqueID(), 100, 0, 1000, 400 + i * 200));
+  {
+    while (++i < 2) {
+      auto e = new Enemy(_party.getUniqueID(), 100, 0, 1000, 400 + i * 200);
+      e->pushAction(Enemy::FOLLOWING);
+      _party.addEnemy(e);
     }
     break;
+  }
   case 4: // 4 enemies
-    while(++i < 4) {
-      _party.addEnemy(new Enemy(_party.getUniqueID(), 100, 0, 1000, 300 + i * 100));
+  {
+    while (++i < 4) {
+      auto e = new Enemy(_party.getUniqueID(), 100, 0, 1000, 300 + i * 100);
+      e->pushAction(Enemy::FOLLOWING);
+      _party.addEnemy(e);
     }
     break;
+  }
   case 5: // 1 mini boss
-    _party.addEnemy(new Enemy(_party.getUniqueID(), 200, 1, 1000, 400));
+  {
+    auto e = new Enemy(_party.getUniqueID(), 200, 1, 1000, 400);
+    e->pushAction(Enemy::FOLLOWING);
+    _party.addEnemy(e);
     break;
+  }
   case 8: // 1 boss
-    _party.addEnemy(new Enemy(_party.getUniqueID(), 250, 2, 1000, 400));
+  {
+    auto e  = new Enemy(_party.getUniqueID(), 250, 2, 1000, 400);
+    e->pushAction(Enemy::FOLLOWING);
+    _party.addEnemy(e);
     break;
+  }
   default: // 1 enemy par defaut
-    _party.addEnemy(new Enemy(_party.getUniqueID(), 100, 0, 1000, 400));
+  {
+    auto e = new Enemy(_party.getUniqueID(), 100, 0, 1000, 400);
+    e->pushAction(Enemy::FOLLOWING);
+    _party.addEnemy(e);
     break;
+  }
   }
   i = -1;
   nb_waves++;
