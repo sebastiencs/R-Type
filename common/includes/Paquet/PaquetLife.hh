deb9401f40195de8a35cdcd427ba8f4acb5ceb4d
Boris Bresciani <boris.bresciani@epitech.eu>
2015-12-19 20:03:28 +0100

    Add PaquetLife

---
+++ common/includes/Paquet/PaquetLife.hh
@@ -0,0 +1,40 @@
+//
+// PaquetLife.hh for epitech in /home/chapui_s/rendu/
+//
+// Made by bresci_b
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Wed Dec  9 16:37:07 2015 bresci_b
+// Last update Wed Dec  9 16:37:07 2015 bresci_b
+//
+
+#ifndef PAQUETLIFE_HH_
+# define PAQUETLIFE_HH_
+
+# include "Paquet.hh"
+
+class		PaquetLife : public Paquet
+{
+public:
+  PaquetLife();
+  PaquetLife(const Buffer &);
+  template <typename T>
+  PaquetLife(const T *data, size_t len) : Paquet(data, len) { parqePaquet(); }
+  virtual ~PaquetLife();
+
+  void		setID(const uint8_t id);
+  void		setLife(const uint8_t life);
+  uint8_t	getID() const;
+  uint8_t	getLife() const;
+  void		createPaquet();
+  void		parsePaquet();
+
+private:
+  uint8_t	_id;
+  uint8_t	_pID;
+  uint8_t	_life;
+};
+
+std::ostream	&operator<<(std::ostream &, PaquetLife &);
+
+#endif /* !PAQUETLIFE_HH_ */
---
+++ common/src/Paquet/PaquetLife.cpp
@@ -0,0 +1,76 @@
+//
+// PaquetLife.hh for epitech in /home/chapui_s/rendu/
+//
+// Made by bresci_b
+// Login   <chapui_s@epitech.eu>
+//
+// Started on  Wed Dec  9 16:37:07 2015 bresci_b
+// Last update Wed Dec  9 16:37:07 2015 bresci_b
+//
+
+#include "PaquetLife.hh"
+
+PaquetLife::PaquetLife()
+  : _id(Paquet::LIFE)
+{
+}
+
+PaquetLife::PaquetLife(const Buffer &buf)
+{
+  size_t	ptr = 0;
+  
+  writeData<char>(ptr, reinterpret_cast<const char *>(buf.get()), buf.size());
+  parsePaquet();
+}
+
+PaquetLife::~PaquetLife()
+{
+}
+
+void		PaquetLife::setID(const uint8_t id)
+{
+  _pID = id;
+}
+
+void		PaquetLife::setLife(const uint8_t life)
+{
+  _life = life;
+}
+
+uint8_t		PaquetLife::getID() const
+{
+  return _pID;
+}
+
+uint8_t	        PaquetLife::getLife() const
+{
+  return _life;
+}
+
+void		PaquetLife::parsePaquet()
+{
+  size_t	ptr = 0;
+
+  _id = readData<uint8_t>(ptr);
+  _pID = readData<uint8_t>(ptr);
+  _life = readData<uint8_t>(ptr);
+}
+
+void		PaqyetLife::createPaquet()
+{
+  size_t	ptr = 0;
+
+  writeData<uint8_t>(ptr, &_id);
+  writeData<uint8_t>(ptr, &_pID);
+  writeData<uint8_t>(ptr, &_life);
+}
+
+std::ostream	&operator<<(std::ostream &os, PaquetLife &p)
+{
+  p.parsePaquet();
+  os << "PaquetLife = "
+     << " { ID : " << (int)p.getID()
+     << ", Life : " << (int)p.getLife()
+     << " };";
+  return (os);
+}
