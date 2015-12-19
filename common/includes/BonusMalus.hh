3a1e213332b543d7a87a980cdb6ee7add2096f70
Boris Bresciani <boris.bresciani@epitech.eu>
2015-12-19 17:18:48 +0100

    Add class BonusMalus (Object)

---
+++ common/includes/BonusMalus.hh
@@ -0,0 +1,37 @@
+//
+// BonusMalus.hh for epitech in /home/bresci_b/R-Type
+//
+// Made by bresci_b
+// Login   <bresci_b@epitech.eu>
+//
+// Started on  Fri Nov 13 00:37:48 2015 bresci_b
+// Last update Fri Nov 13 00:37:48 2015 bresci_b
+//
+
+#ifndef BONUSMALUS_HH_
+# define BONUSMALUS_HH_
+
+# include <memory>
+# include "Debug.hh"
+# include "Object.hh"
+
+class		BonusMalus : public Object
+{
+public:
+  BonusMalus();
+  BonusMalus(const uint8_t type, const uint8_t speed, const uint16_t x, const uint16_t y);
+  virtual ~BonusMalus();
+  
+  uint8_t	getSpeed() const;
+  void		setSpeed(const uint8_t speed);
+  uint8_t	getType() const;
+  void		setType(const uint8_t type);
+  
+private:
+  uint8_t	_speed;
+  uint8_t	_type;
+};
+
+typedef std::shared_ptr<BonusMalus> BonusMalus_SharedPtr;
+  
+#endif /* !BONUSMALUS_HH_ */
---
+++ common/src/BonusMalus.cpp
@@ -0,0 +1,52 @@
+//
+// BonusMalus.cpp for epitech in /home/bresci_b/R-Type
+//
+// Made by bresci_b
+// Login   <bresci_b@epitech.eu>
+//
+// Started on  Fri Nov 13 00:37:48 2015 bresci_b
+// Last update Fri Nov 13 00:37:48 2015 bresci_b
+//
+
+#include "BonusMalus.hh"
+
+BonusMalus::BonusMalus()
+  : Object()
+{
+  _type = 0;
+  _speed = 0;
+  DEBUG_MSG("BonusMalus created");
+}
+
+BonusMalus::BonusMalus(const uint8_t type, const uint8_t speed, const uint16_t x, const uint16_t y)
+  : Object(0, x, y, 0, 0)
+{
+  _type = type;
+  _speed = speed;
+  DEBUG_MSG("BonusMalus created");
+}
+
+BonusMalus::~BonusMalus()
+{
+  DEBUG_MSG("BonusMalus deleted");
+}
+
+uint8_t		BonusMalus::getSpeed() const
+{
+  return _speed;
+}
+
+void		BonusMalus::setSpeed(const uint8_t speed)
+{
+  _speed = speed;
+}
+
+uint8_t		BonusMalus::getType() const
+{
+  return _type;
+}
+
+void		BonusMalus::setType(const uint8_t type)
+{
+  _type = type;
+}
