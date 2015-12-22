8ee489631a5680602d3cdeb86e75232de79cd89c
Clément <clement.da-silva@epitech.eu>
2015-12-22 22:45:25 +0100

    Added option to change inputmode in graphicEngine

--- client/includes/IGraphicEngine.hh
+++ client/includes/IGraphicEngine.hh
@@ -27,6 +27,12 @@ enum UsableKeys {
 	SPACE
 };
 
+enum InputMode {
+	ZQSD = 0,
+	ARROWS,
+	PAD
+};
+
 typedef std::function<void()> callback;
 typedef std::function<void(uint32_t, uint32_t)> mouseCallback;
 typedef std::function<void(const char)> textEnteredCallback;
@@ -53,11 +59,10 @@ public:
 	virtual void setTextEnteredCallback(textEnteredCallback) = 0;
 	virtual void setUsableKeyPressedCallback(usableKeyPressedCallback) = 0;
 
+	virtual void setInputMode(InputMode) = 0;
 	virtual void handleEvents() = 0;
 	virtual void launch() = 0;
 
-	//virtual int32_t getDeltaTimeMS() = 0;
-	//virtual float getDeltaTimeS() = 0;
 	virtual int getWindowWidth() const = 0;
 	virtual int getWindowHeight() const = 0;
 
